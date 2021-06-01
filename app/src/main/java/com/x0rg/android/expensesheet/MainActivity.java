56dd456841cb653e7f6e1330926aac48911958d6
Nikhil Gupta <nikhil062000@gmail.com>
2021-06-02 01:45:33 +0530

    added list_item_details activity

--- app/src/main/java/com/x0rg/android/expensesheet/MainActivity.java
+++ app/src/main/java/com/x0rg/android/expensesheet/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -57,6 +58,17 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.list);
 
         listView.setAdapter(itemsAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                SummaryDay day = days.get(position);
+
+                Intent dayIntent = new Intent(MainActivity.this, DayDetails.class);
+                startActivity(dayIntent);
+
+            }
+        });
 
     }
 }
