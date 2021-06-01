261cdd2a32819b566f3a76d9cc4b7641c12cb503
Nikhil Gupta <nikhil062000@gmail.com>
2021-06-01 16:30:22 +0530

    added button AddExpense/Income and aligned the layout

--- app/src/main/java/com/x0rg/android/expensesheet/MainActivity.java
+++ app/src/main/java/com/x0rg/android/expensesheet/MainActivity.java
@@ -21,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
         days.add(new SummaryDay(7,8,9));
         days.add(new SummaryDay(10,11,12));
         days.add(new SummaryDay(100,200,300));
+        days.add(new SummaryDay(12, 13,14));
+        days.add(new SummaryDay(10,11,12));
+        days.add(new SummaryDay(5,6,7));
+        days.add(new SummaryDay(7,8,9));
+        days.add(new SummaryDay(10,11,12));
+        days.add(new SummaryDay(100,200,300));
 
         SummaryDayAdapter itemsAdapter = new SummaryDayAdapter(this, days);
 
